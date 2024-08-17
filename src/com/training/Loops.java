fca5372721a50e53b44255c75ebb872c731afedb
[Muqadas Khan] <[muqadask2021@gmail.com]>
2024-08-16 23:40:16 -0400

    Loops

---
+++ src/com/training/Loops.java
@@ -0,0 +1,28 @@
+package com.training;
+
+
+
+   
+    	public class Loops {
+    	    public static void main(String[] args) {
+    	        for (int i = 0; i <= 100; i++) {
+    	            if (i % 2 != 0) { // Check if the number is odd
+    	                System.out.println(i);
+    	                
+    	            }
+    	        }
+    	        System.out.println("=============================================");
+    	        for (int i = 0; i <= 100; i++) {
+    	            if (i % 2 == 0) { // Check if the number is even
+    	                System.out.println(i);
+    	            }
+    	        }  
+    	        
+    	    }
+    	}
+//    	public class Loops1 {
+//    	    public static void main(String[] args) {
+//    	        System.out.println("Even Numbers:");
+//
+//    	    }
+//    	}
